e49adeefbfd62d165012b36252a33912de258d57
Mustafa571 <Mustafa571@users.noreply.github.com>
2016-04-30 07:32:56 +0200

    Update default.py

--- default.py
+++ default.py
@@ -85,32 +85,32 @@ elif mode[0] == 'folder':
 			li = xbmcgui.ListItem(channelname, iconImage='http://i.huffpost.com/gen/2385860/images/o-STREAMING-facebook.jpg')
 			xbmcplugin.addDirectoryItem(handle=addon_handle, url=channelurl, listitem=li)
 			
-        if foldername == "Filmler":
+        elif foldername == "Filmler":
 		r = requests.get("https://raw.githubusercontent.com/Mustafa571/VATAN/master/Filmler.txt")
 		match = re.compile('(.+)\*(.+)').findall(r.content)
 		for channel in match:
 			channelname = channel[0]
-			channelurl = channel[1].replace(' ',"")
+			channelurl = channel[2].replace(' ',"")
 			channelurl = ''.join(channelurl.splitlines())
 			li = xbmcgui.ListItem(channelname, iconImage='http://profesyonellogo.com/wp-content/uploads/2012/10/sinema_LOGO.jpg')
 			xbmcplugin.addDirectoryItem(handle=addon_handle, url=channelurl, listitem=li)
 			
-        if foldername == "Diziler":
+        elif foldername == "Diziler":
 		r = requests.get("https://raw.githubusercontent.com/Mustafa571/VATAN/master/Diziler.txt")
 		match = re.compile('(.+)\*(.+)').findall(r.content)
 		for channel in match:
 			channelname = channel[0]
-			channelurl = channel[1].replace(' ',"")
+			channelurl = channel[3].replace(' ',"")
 			channelurl = ''.join(channelurl.splitlines())
 			li = xbmcgui.ListItem(channelname, iconImage='https://lh5.ggpht.com/8LStBl9nroWvyl5Rfxd0f0TneWtL1Yu7HFI4-hBsr-xKhTmtKKdg_viioYfZ1hMJnO4a')
 			xbmcplugin.addDirectoryItem(handle=addon_handle, url=channelurl, listitem=li)
 			
-        if foldername == "Alman Kanalları":
+        elif foldername == "Alman Kanalları":
 		r = requests.get("https://raw.githubusercontent.com/Mustafa571/VATAN/master/Alman%20Kanallari.txt")
 		match = re.compile('(.+)\*(.+)').findall(r.content)
 		for channel in match:
 			channelname = channel[0]
-			channelurl = channel[1].replace(' ',"")
+			channelurl = channel[4].replace(' ',"")
 			channelurl = ''.join(channelurl.splitlines())
 			li = xbmcgui.ListItem(channelname, iconImage='https://andronews.de/wp-content/uploads/2014/04/screenshot-140425-153659-1024x455.jpg')
 			xbmcplugin.addDirectoryItem(handle=addon_handle, url=channelurl, listitem=li)
